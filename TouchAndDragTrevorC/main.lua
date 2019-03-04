a1b44a7c1557cc492c14a51635ace4812cbf81ba
Trevor Cosby <trevor.cosby@ocsbstudent.ca>
2019-03-04 09:28:34 -0500

    updates unit1

--- TouchAndDragTrevorC/main.lua
+++ TouchAndDragTrevorC/main.lua
@@ -1,7 +1,9 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndDrag
+-- Name: TrevorC
+-- Course: ICS2O/3C
+-- This program...has 2 images on the screen when i click and drag on each image
+--it moves with my finger
 
--- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+
+local backgroundImage = display.newImageRect("Images/Background.png", 2048, 1536)
