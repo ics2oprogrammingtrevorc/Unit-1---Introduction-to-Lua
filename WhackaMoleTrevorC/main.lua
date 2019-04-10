39c2ceac5d95aa643e2cf639a53f7fc49d98c19a
Trevor Cosby <47821639+s448315@users.noreply.github.com>
2019-04-10 09:45:42 -0400

    whackamole

--- WhackaMoleTrevorC/main.lua
+++ WhackaMoleTrevorC/main.lua
@@ -7,6 +7,10 @@
 -- Your code here
 display.setStatusBar(display.HiddenStatusBar)
 
+------------------------------------------------------------------------------------------
+--VARIABLES
+------------------------------------------------------------------------------------------
+
 local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight)
 bkg:setFillColor( 0.3, 0.8, 0.2)
 
@@ -16,3 +20,21 @@ bkg.x = 0
 bkg.y = 0
 
 local mole = display.newImage( "Images/mole.png", 0, 0)
+mole.x = display.contentCenterX
+mole.y = display.contentCenterY
+mole:scale(0.33, 0.33)
+mole.isVisible = false
+local scoreText
+local score
+
+---------------------------------------------------------------------------------------------
+--FUNTIONS
+---------------------------------------------------------------------------------------------
+
+function PopUp ()
+
+	mole.x = math.random( 0, display.contentWidth )
+	mole.y = math.random( 0 , display.contentHeight)
+	mole.isVisible = true
+	timer.performWithDelay (500, Hide( ))
+end
