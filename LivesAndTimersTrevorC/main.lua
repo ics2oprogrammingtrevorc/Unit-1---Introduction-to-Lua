3c9860f2abb8bd44804f386c37be6858d93dbff7
Trevor Cosby <47821639+s448315@users.noreply.github.com>
2019-04-17 08:58:11 -0400

    finish whackamole

--- LivesAndTimersTrevorC/main.lua
+++ LivesAndTimersTrevorC/main.lua
@@ -13,7 +13,7 @@ local heart1
 local heart2
 
 ---------------------------------------------------------------------------------------
--- LOCAL FUNCTIONS
+--------------------  LOCAL FUNCTIONS  --------------------------------------
 ---------------------------------------------------------------------------------------
 
 local function updateTime()
@@ -28,4 +28,33 @@ local function updateTime()
 		secondsLeft = totalSeconds
 		lives = lives - 1
 
-		
+		if (lives == 3) then
+			heart3.isVisible = false
+		elseif (lives == 2) then
+			heart2.isVisible = false
+		elseif  (lives == 1) then
+			heart1.isVisible = false
+		end
+
+	end
+end
+
+local function StartTimer()
+	countdownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+end
+
+-------------------------------------------------------------------------
+---------------------------  OBJECT CREATION  ------------------------------------
+-------------------------------------------------------------------------
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentWidth * 7 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentWidth * 6 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
--- WhackaMoleTrevorC/main.lua
+++ WhackaMoleTrevorC/main.lua
@@ -1,40 +1,69 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: WhackAMole
+-- Name: TrevorC
+-- Course: ICS2O/3C
+-- This program... Creates a game of whackamole
 -----------------------------------------------------------------------------------------
 
--- Your code here
 display.setStatusBar(display.HiddenStatusBar)
 
-------------------------------------------------------------------------------------------
---VARIABLES
-------------------------------------------------------------------------------------------
+--------------------------------------------------------VARIABLES-------------------------------------------
 
 local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight)
-bkg:setFillColor( 0.3, 0.8, 0.2)
+bkg:setFillColor( 0.7, 0.2, 0.4)
 
 bkg.anchorX = 0
 bkg.anchorY = 0
 bkg.x = 0
 bkg.y = 0
 
-local mole = display.newImage( "Images/mole.png", 0, 0)
+local mole = display.newImage( "images/mole.png", 0, 0)
 mole.x = display.contentCenterX
 mole.y = display.contentCenterY
-mole:scale(0.33, 0.33)
+mole:scale(2, 2)
 mole.isVisible = false
 local scoreText
-local score
+local textSize = 50
 
----------------------------------------------------------------------------------------------
---FUNTIONS
----------------------------------------------------------------------------------------------
+--------------------------------------------------------FUNTIONS--------------------------------------------------------------------
 
-function PopUp ()
+function PopUp ( )
 
 	mole.x = math.random( 0, display.contentWidth )
 	mole.y = math.random( 0 , display.contentHeight)
 	mole.isVisible = true
-	timer.performWithDelay (500, Hide( ))
+	timer.performWithDelay (5000, Hide)
 end
+
+function PopUpDelay( )
+	timer.performWithDelay( 3000, PopUp)
+end
+
+function Hide( )
+	mole.isVisible = false
+	PopUpDelay( )
+end
+
+function GameStart( )
+	PopUpDelay( )
+end
+
+function Whacked( event )
+	if (event.phase == "began") then
+		scoreText.text = scoreText.text + 1
+	end
+end
+
+------------------------OBJECTS----------------------------------------------------------------------------------------
+
+scoreText = display.newText("0", display.contentWidth/2, display.contentHeight*2/3, nil, 400)
+scoreText.y = display.contentHeight *4/7
+scoreText.x = display.contentWidth * 1/7
+scoreText:setTextColor(.4, .5, .6)
+scoreText.isVisible = true
+
+----------------------------EVENT LISTENERS-------------------------------------------------------------------------------------------
+mole:addEventListener( "touch", Whacked )
+
+----------------------------START THE GAME ---------------------------------------------------------------------------------------------
+
+GameStart()
